d60f85eb3083e52aa1bdbf5e8fdec33bf43de1e1
ashikameer <ashikameer100@gmail.com>
2017-03-12 15:10:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,9 +9,64 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var articleOne={
+    title:'Article One',
+    heading:'Article One',
+    date:' Mar 13,2017',
+    content:`<p>
+                This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.
+            </p>
+            <p>
+                This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.
+            </p>
+            <p>
+                    This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.
+            </p>
+            
+            <p>
+                This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.
+            </p>
+            <p>
+                This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.
+            <p>
+                    This is Article one.This is Article one.This is Article one.This is Article one.This is Article one.
+            </p>`,
+};
+function createTemp(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    var htmlTemplate=`<html>
+                            <head>
+                                <title>
+                                   ${title}
+                                </title>
+                                <link href="/ui/style.css" rel="stylesheet"/>
+                            </head>
+                            <body>
+                                <div>
+                                    <a href="/">Home</a>
+                                </div>
+                                <hr/>
+                                <h3>
+                                    ${heading}
+                                </h3>
+                                <div>
+                                    ${date}   
+                                </div>
+                                <div class="container">
+                                    ${content}
+                                </div>
+                            </body>
+                        </html>`
+    return htmlTemplate;                    
+} 
+
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemp(articleOne));
 });
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
