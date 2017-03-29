c3af5612ca097b74efd1c4d983915294b125faaf
ashikameer <ashikameer100@gmail.com>
2017-03-29 17:29:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,4 @@
-var img = document.getElementById('madi') ;
-var lft=0 ;
-function moveRight () {
-    lft=lft+10;
-    img.style.marginLeft = lft + 'px'; 
-}
+ 
 
 img.onclick = function () {
     var interval =setInterval(moveRight,100);
