c2c9c5ae89c4e37c3459aea07820adf53e349cd3
ashikameer <ashikameer100@gmail.com>
2017-03-31 11:27:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
-butto=document.getElementById('counter');
+var butto=document.getElementById('counter');
 var counter = 0 ;
 butto.onclick===function(){
     counter=counter+1;
-    spa=document.getElementById('count');
+    var spa=document.getElementById('count');
     spa.innerHTML=counter.toString();
 };
