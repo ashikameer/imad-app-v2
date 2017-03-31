5986a2dfeb9113c95705eee59046ee11037bbc5f
ashikameer <ashikameer100@gmail.com>
2017-03-31 23:46:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,11 +7,11 @@ button.onclick=function(){
         if(request.readystate===XMLHttpRequest.DONE){
             if(request.status===200){
                 var counter = request.responseText;
-                var span = document.getElementById('count')
-                span.innerHTML=counter.toString();
+                var span = document.getElementById('count');
+                span.innerHTML=counter;
             }
         }
-    }
+    };
     request.open('GET','http://ashikameer.imad.hasura-app.io/counter',true);
-    request.send(null)
+    request.send(null);
 };
