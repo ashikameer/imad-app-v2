5315011a434986df49a6534d08670e9041156ce9
ashikameer <ashikameer100@gmail.com>
2017-04-01 13:03:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,4 +16,26 @@ button.onclick=function(){
     request.send(null);
 };
 
-var submit= document.getElementById("subit.btn");
+var list=[];
+var submit= document.getElementById("submit.btn");
+submit.onclick=function(){
+    var inp = document.getElementById('name');
+    var name=inp.value;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var names = request.responseText;
+                for(var i=0;i<names.length;i++){
+                    list="<li>"+names[i]+"</li>";
+                }
+                var upd = document.getElementbyId("lis");
+                upd.innerHTML = list;
+            }
+        }
+    };
+    request.open('GET','http://ashikameer.imad.hasura-app.io/submit-name:'+name,true);
+    request.send(null);
+    
+};
+
