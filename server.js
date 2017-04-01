82c97150112affbff8edd539999225d023dc8958
ashikameer <ashikameer100@gmail.com>
2017-04-01 12:40:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+var names=[];
+app.get('/submit-name:name', function (req, res) {
+  var name=req.params.name;
+  names.push(name);
+  res.send(JSON.stringify(names));
+});
 app.get('/counter', function (req, res) {
   counter=counter+1 ;
   res.send(counter.toString());
